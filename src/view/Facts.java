d4583f362ccd097e3b9d1ec39b122341353ae309
Tehila <tehilola1@gmail.com>
2020-01-16 09:54:26 +0200

    did you know

--- src/view/Facts.java
+++ src/view/Facts.java
@@ -12,6 +12,8 @@ import java.awt.event.ActionListener;
 
 import static java.lang.System.exit;
 
+import model.Question_Answer;
+
 public class Facts extends JDialog{
     Color color = new Color(131,24 ,11);
     public Facts(Frame owner, boolean modal, int score, queue_question q) {
@@ -27,28 +29,31 @@ public class Facts extends JDialog{
         contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
         setContentPane(contentPane);
         contentPane.setLayout(null);
-        // todo hayadata
-//        JLabel messageLabel = new JLabel("<html>question</html>");
-//        messageLabel.setBackground(color);
-//        messageLabel.setForeground(Color.WHITE);
-//        messageLabel.setFont(new Font("Tahoma", Font.BOLD, height / 20));
-//        messageLabel.setBorder(new MatteBorder(2, 2, 2, 2, (Color) color.ORANGE));
-//        messageLabel.setSize(new Dimension(height / 2, height / 3));
-//        messageLabel.setLocation(width / 10, width / 6);
-//        messageLabel.setHorizontalAlignment(SwingConstants.CENTER);
-//        messageLabel.setVerticalAlignment(SwingConstants.CENTER);
-//        contentPane.add(messageLabel);
+        //did you know:
+        Question_Answer qu=new Question_Answer("what","tr","paz","it is a wonderful game:) :) i love it!");
+        String know=qu.getGood_to_know();
+        JLabel messageLabel = new JLabel("<html><div style='text-align: center;'>"+"good to know:" +"<br>"+know+"</html>");
+        messageLabel.setBackground(color);
+        messageLabel.setForeground(Color.orange);
+        messageLabel.setFont(new Font("Tahoma", Font.BOLD, height / 18));
+        Color color1 = new Color(146, 45, 18);
+        //messageLabel.setBorder(new MatteBorder(2, 2, 2, 2,);
+        messageLabel.setSize(new Dimension(height , height / 3-20));
+        messageLabel.setLocation(width / 3-80, width / 3+2);
+        messageLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        messageLabel.setVerticalAlignment(SwingConstants.CENTER);
+        contentPane.add(messageLabel);
 
         int textFieldHeight = height / 30;
         JButton exitGame = new JButton("exit game");
         //Color color1 = new Color(146,45,18);
-        Color color1 = new Color(146, 45, 18);
+        //Color color1 = new Color(146, 45, 18);
         exitGame.setBackground(color1);
         exitGame.setForeground(Color.ORANGE);
         exitGame.setFont(new Font("Tahoma", Font.BOLD, height / 15));
-        exitGame.setBorder(new MatteBorder(2, 2, 2, 2, (Color) color.ORANGE));
-        exitGame.setSize(new Dimension(height / 2, height / 2));
-        exitGame.setLocation(height / 6, width / 6);
+        exitGame.setBorder(new MatteBorder(2, 2, 2, 2, (Color) color.PINK));
+        exitGame.setSize(new Dimension(height / 2, height / 2-65));
+        exitGame.setLocation((height / 6)+30, (width / 6)-30);
         exitGame.setFocusPainted(false);
         exitGame.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent arg0) {
@@ -61,9 +66,9 @@ public class Facts extends JDialog{
         menuButton.setBackground(color1);
         menuButton.setForeground(Color.ORANGE);
         menuButton.setFont(new Font("Tahoma", Font.BOLD, height / 15));
-        menuButton.setBorder(new MatteBorder(2, 2, 2, 2, (Color) color.ORANGE));
-        menuButton.setSize(new Dimension(height / 2, height / 2));
-        menuButton.setLocation(height , width / 6);
+        menuButton.setBorder(new MatteBorder(2, 2, 2, 2, (Color) color.PINK));
+        menuButton.setSize(new Dimension(height / 2, height / 2-65));
+        menuButton.setLocation(height+10 , (width / 6)-30);
         menuButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent arg0) {
                 setVisible(false);
@@ -71,11 +76,35 @@ public class Facts extends JDialog{
                 frame.setVisible(true);
             }
         });
+//        JButton knowButton = new JButton("good to know");
+//        //Color color1 = new Color(146,45,18);
+//        knowButton.setBackground(color1);
+//        knowButton.setForeground(Color.ORANGE);
+//        knowButton.setFont(new Font("Tahoma", Font.BOLD, height / 15));
+//        knowButton.setBorder(new MatteBorder(2, 2, 2, 2, (Color) color.ORANGE));
+//        knowButton.setSize(new Dimension(height / 3, height / 3));
+//        knowButton.setLocation(height+100 , width / 6);
+//        knowButton.addActionListener(new ActionListener() {
+//            public void actionPerformed(ActionEvent arg0) {
+//                setVisible(false);
+//                MainWindow frame = new MainWindow(q);
+//                frame.setVisible(true);
+//            }
+//        });
+       // String know= Question_Answer q
+//        JLabel good_to_know = new JLabel("quicky");
+//        good_to_know.setForeground(new Color(146,45,18));
+//        good_to_know.setFont(new Font("Ariel", Font.BOLD, height /3));
+//        good_to_know.setSize(good_to_know.getPreferredSize());
+//        good_to_know.setLocation((width - good_to_know.getWidth()) / 2-20, height / 20-28);
+//        contentPane.add(good_to_know);
+
+
         JLabel scoreLabel = new JLabel("<html>" + "your score is: "+score + "</html>");
         scoreLabel.setBackground(color);
         scoreLabel.setForeground(Color.WHITE);
-        scoreLabel.setFont(new Font("Tahoma", Font.BOLD, height / 12));
-        scoreLabel.setSize(new Dimension(width/2+450, height/3 ));
+        scoreLabel.setFont(new Font("Tahoma", Font.BOLD, height / 13));
+        scoreLabel.setSize(new Dimension(width/2+345, height/3-70 ));
         //scoreLabel.setLocation(width / 2-200, 20);
         scoreLabel.setHorizontalAlignment(SwingConstants.CENTER);
         scoreLabel.setVerticalAlignment(SwingConstants.CENTER);
