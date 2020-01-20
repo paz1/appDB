d5ba42d85fc48083d0cd99f7c9756852389d20a1
Tehila <tehilola1@gmail.com>
2020-01-20 13:07:32 +0200

    general

--- src/view/General.java
+++ src/view/General.java
@@ -5,32 +5,11 @@ import java.awt.Toolkit;
 
 public class General {
     /** width of the main window */
-   // final static public int width = Toolkit.getDefaultToolkit().getScreenSize().width / 2;
     final static public int width = 960;
     /** height of the main window */
     final static public int height = 540;
-    //final static public int height = Toolkit.getDefaultToolkit().getScreenSize().height /2;
     final static public int screenWidth = Toolkit.getDefaultToolkit().getScreenSize().width;
-    //final static public int screenWidth = 1920;
     final static public int screenHeight = Toolkit.getDefaultToolkit().getScreenSize().height;
-    //final static public int screenHeight = 1080;
-//    /** the hight of label */
-    //final static public int labelHeight = height / 40;
-//    /** the hight of a field */
-//    final static public int fieldHeight = labelHeight + 10;
-//    /** the difference Between Lines in dialogs */
-//    final static public int differenceBetweenLines = (int)(1.4 * fieldHeight);
-//    /** default field Width */
-//    final static public int fieldWidth = width / 11;
-//    /** default label Width */
-//    //final static public int labelWidth = width / 12;
-//    /** default font */
-//    Font font;
-//    public General()
-//    {
-//        int screenRes = Toolkit.getDefaultToolkit().getScreenResolution();
-//        int fontSize = (int)Math.round(12.0 * screenRes / 92.0);
-//        font = new Font("Arial", Font.PLAIN, fontSize);
-//    }
+
 
 }
