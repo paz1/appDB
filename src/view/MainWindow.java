2c6de93610b2be15256a47d4797fe878d6c51d7f
adishani15 <adishani15@gmail.com>
2020-01-21 14:38:06 +0200

    a

--- src/view/MainWindow.java
+++ src/view/MainWindow.java
@@ -12,11 +12,17 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
 
 public class MainWindow extends JFrame {
 	Color color = new Color(89,131 ,130);
 	Game g;
 	controller c;
+
+
+
 	/**
 	 * Launch the application.
 	 */
@@ -92,6 +98,8 @@ public class MainWindow extends JFrame {
 	 * Create the frame.
 	 */
 	public MainWindow(controller controller,queue_question queue_question) {
+
+
 		super("quicky");
 		c=controller;
 		setBackground(color);
@@ -104,9 +112,12 @@ public class MainWindow extends JFrame {
 			protected void paintComponent(Graphics g) {
 				super.paintComponent(g);
 				Image image =null;
+
+
 				try {
-					image = ImageIO.read(new File("./image.jpg"));
-				} catch (IOException e) {
+					InputStream inputStream = ClassLoader.getSystemClassLoader().getResourceAsStream("image.png");
+					image = ImageIO.read(inputStream);
+				} catch (Exception e) {
 					e.printStackTrace();
 				}
 				g.drawImage(image, 0, 0, null);
